82686cc9a1a554105d85c8a02434ea008d8742a6
F. Ria Khan <friak42@gmail.com>
2023-12-07 22:30:43 -0800

    I got controllers to work-Donny
    Controller joysticks are functional, mic still a bit of trouble tho. currently working on trying to get python to unity

--- ArduinoCode/Attempt Dawn/arduino controllers.py
+++ ArduinoCode/Attempt Dawn/arduino controllers.py
@@ -1,8 +1,14 @@
 from pyfirmata import Arduino, util
 from pyfirmata.util import Iterator
-#import qwiic_joystick
 import time
 from math import floor
+from vosk import Model, KaldiRecognizer
+import pyaudio
+model = Model(r"C:\Users\Khan\Documents\GitHub\Shouting_Match\ArduinoCode\Attempt Dawn\test en")
+recognizer = KaldiRecognizer(model,16000)
+capt= pyaudio.PyAudio()
+stream = capt.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=8192)
+stream.start_stream()
 
 try:
     board = Arduino('COM3')
@@ -14,24 +20,34 @@ except:
     print("ERROR: Could Not Connect to Arduino Board")
     board = None
     exit()
-#myJoystick = qwiic_joystick.QwiicJoystick()
+
 joystick_x = board.get_pin("a:4:i")
 joystick_y = board.get_pin("a:5:i")
+joystick_s = board.get_pin("a:2:i")
+joystick_shout = board.get_pin("a:0:i")
 joystick_x.enable_reporting()
 joystick_y.enable_reporting()
+joystick_s.enable_reporting()
+joystick_shout.enable_reporting()
 
-dt = 0.1
+dt = .1
 
 #val_x, val_y, val_s = .5, .5, 0
 
-#joystick_switch.write(1)
+
 
 while True:
-    time.sleep(dt)
+    time.sleep(dt*5)
     val_x = joystick_x.read()
     val_y = joystick_y.read()
-    #val_s = floor(joystick_switch.read())
+    val_s = joystick_s.read()
+    val_shout = joystick_shout.read()
+    print('X',val_x, 'Y', val_y, 'Button',val_s)
+    #data = stream.read(9600)
+    #if recognizer.AcceptWaveform(data):
+        #print(recognizer.Result())
+    #val_s = floor(joystick_s.read())
     # if val_x is None or val_y is None or val_s:
     #     val_x, val_y, val_s = .5, .5, 0
-    print('X',val_x, 'Y', val_y)
+    print('X',val_x, 'Y', val_y, 'Button',val_s,'shout',val_shout)
    
