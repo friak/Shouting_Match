22af28b2863e3c8df50868d1d0f87fa60e43122f
Wozzyb <shafikd@uci.edu>
2024-01-18 13:43:30 -0800

    Fixed Movement
    Updated some of the objects / playable characters to fix their movements.

--- ShoutingMatch/Assets/3D Scripts/PlayerController.cs
+++ ShoutingMatch/Assets/3D Scripts/PlayerController.cs
@@ -223,6 +223,7 @@ public class
     }
     private void Forward()
     {
+        Debug.Log("forward mode");
         if (isForward && !isForwardAnimation)
         {
             animator.SetBool("isForward", true);
@@ -245,6 +246,7 @@ public class
         {   // standard attack
             if (IsIdle() || isJumpingPressed)
             {
+                Debug.Log("idle mode");
                 animator.SetTrigger("attack1");
                 attack.StartAttack(standardAttack, level);
                 animator.ResetTrigger("attack1");
